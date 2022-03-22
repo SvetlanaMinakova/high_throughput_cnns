64be3b588ab79ee951c633ca56f89de84d0fc1e3
Svetlana <svetlanaminakova42@gmail.com>
2022-03-22 20:20:38 +0100

    API files and integration test update

--- tests/t_unit.py
+++ tests/t_unit.py
@@ -6,16 +6,18 @@ import sys
 
 
 def main():
-    """ Run the whole pipeline for example mnist CNN and example jetson TX2 platform"""
+    """ Run parts of the pipeline for example CNN and example platform, defined in ../test_config.py"""
     # import current directory and it's subdirectories into system path for the current console
     # this would allow importing project modules without adding the project to the PYTHONPATH
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     sys.path.append(root_dir)
 
     # general arguments
-    parser = argparse.ArgumentParser(description='The script runs an integration (end-to-end) tests for an example '
-                                                 './input_examples/dnn/mnist.onnx CNN and an example '
-                                                 './input_examples/architecture/jetson.json platform')
+    parser = argparse.ArgumentParser(description='The script runs unit (per-step) tests for an example '
+                                                 'CNN and an example platform, defined in ./test_config.py')
+    parser.add_argument("-s", "--step", type=str, action='store', required=True,
+                        help='Script (step) to test. Select from [dnn_to_sdf,'
+                             'sdf_latency_eval_template, generate_mapping, generate_final_app]')
 
     parser.add_argument('--info-level', metavar='--info-level', type=int, action='store', default=1,
                         help='Info-level, i.e., amount of information to print out during the tests run. '
@@ -28,52 +30,20 @@ def main():
     try:
         args = parser.parse_args()
         info_level = args.info_level
+        step = args.step
 
         # import project modules
         from util import get_project_root
         from fileworkers.common_fw import clear_folder, create_or_overwrite_dir
         from tests.test_config import get_test_config
 
-        config = get_test_config()
-        # cleanup intermediate files directory
-        # print("cleanup folder", intermediate_files_folder_abs)
-        clear_folder(config["intermediate_files_folder_abs"])
-        create_or_overwrite_dir(config["intermediate_files_folder_abs"])
-        integration_test_result = run_integration_test(info_level)
-        return integration_test_result
+        unit_test_result = run_test_step(step, info_level)
+        return unit_test_result
 
     except Exception as e:
         print(" Integration tests error: " + str(e))
         traceback.print_tb(e.__traceback__)
         return 1
-
-
-def run_integration_test(info_level):
-    """
-    Run integration tests
-    :param info_level: amount of information to print out during the tests run.
-        If info-level == 0, no information is printed to console.
-        If info-level == 1, only tests information (e.g., which steps of the tests were successful)
-        is printed to console.
-        If info-level == 2, tests information (e.g., which steps of the tests were successful)
-        as well as script-specific verbose output is printed to the console
-    :return: True if tests ran successfully and False otherwise
-    """
-    if info_level > 0:
-        print("RUN INTEGRATION TEST")
-    # steps (scripts) in execution order
-    steps = ["dnn_to_sdf",
-             "sdf_latency_eval_template",
-             "generate_mapping",
-             "generate_final_app"]
-    for step in steps:
-        step_executed = run_test_step(step, info_level)
-        if step_executed is False:
-            if info_level > 0:
-                print("INTEGRATION TEST FAILED")
-                return 1
-    print("INTEGRATION TEST PASSED")
-    return 0
 
 
 def run_test_step(step: str, info_level):
