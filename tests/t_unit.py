f692fe4af7439a5fe6cf83f0a4c7e34eccc192bf
Svetlana <svetlanaminakova42@gmail.com>
2022-05-03 20:43:59 +0200

    generic functions for unit tests are now in separate script

--- tests/t_unit.py
+++ tests/t_unit.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import subprocess
 import traceback
 import sys
 
@@ -209,6 +208,8 @@ def run_dnn_to_sdf_task_graph(config: {}, info_level):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "dnn_to_sdf_task_graph"
     input_param = {
@@ -249,6 +250,8 @@ def run_sdf_latency_eval_template(config, info_level):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "sdf_latency_eval_template"
 
@@ -317,6 +320,8 @@ def run_generate_mapping(config, info_level):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_mapping"
 
@@ -387,6 +392,8 @@ def run_generate_final_app(config, info_level):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_final_app_model"
     input_param = {
@@ -457,6 +464,8 @@ def run_generate_code_wrapper(config, info_level, print_prefix="  "):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_code_wrapper"
 
@@ -529,6 +538,8 @@ def run_generate_code_arm_cl(config, info_level, print_prefix="  "):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_code_arm_cl"
 
@@ -599,6 +610,8 @@ def run_generate_code_tensorrt(config, info_level, print_prefix="  "):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_code_tensorrt"
 
@@ -668,6 +681,8 @@ def run_generate_code_mixed(config, info_level, print_prefix="  "):
 
     # import project modules
     from util import get_project_root
+    from t_unit_common import run_script_and_check_output
+
     script_root = get_project_root()
     script_name = "generate_code_mixed"
 
@@ -689,104 +704,7 @@ def run_generate_code_mixed(config, info_level, print_prefix="  "):
     return test_passed
 
 
-def run_script_and_check_output(script_root,
-                                script_name,
-                                input_param: {},
-                                flags: [],
-                                output_file_paths: [],
-                                info_level):
-    """
-    Run script and check whether it was properly executed.
-    If the script was properly executed it would generate a specific set of output files.
-    :param script_root: folder, where script is located
-    :param script_name: name of the script
-    :param input_param: (dictionary) list of input parameters, where key=parameter name,
-        value = parameter value
-    :param flags: list of input flags
-    :param output_file_paths: list of absolute paths to expected output files
-    :param info_level: amount of information to print out during the tests run.
-        If info-level == 0, no information is printed to console.
-        If info-level == 1, only tests information (e.g., which steps of the tests were successful)
-        is printed to console.
-        If info-level == 2, tests information (e.g., which steps of the tests were successful)
-        as well as script-specific verbose output is printed to the console
-    :return: True, if the script was successfully executed and
-        all the output files was generated, False otherwise
-    """
-    # run script
-    script_successfully_executed = run_script(script_root, script_name, input_param, flags, info_level)
-    if info_level > 0:
-        print("  - script successfully executed:", script_successfully_executed)
-
-    # check the output files
-    output_files_generated = files_exist(output_file_paths)
-    if info_level > 0:
-        print("  - script output files are successfully generated:", output_files_generated)
-
-    test_passed = True if script_successfully_executed and output_files_generated else False
-    if info_level > 0:
-        if test_passed:
-            print("TEST PASSED")
-        else:
-            print("TEST FAILED")
-    return test_passed
-
-
-def run_script(script_root, script_name, input_param: {}, flags: [], info_level):
-    """
-    Run script
-    :param script_root: folder, where script is located
-    :param script_name: name of the script
-    :param input_param: (dictionary) list of input parameters, where key=parameter name,
-        value = parameter value
-    :param flags: list of input flags
-    :param info_level: amount of information to print out during the tests run.
-        If info-level == 0, no information is printed to console.
-        If info-level == 1, only tests information (e.g., which steps of the tests were successful)
-        is printed to console.
-        If info-level == 2, tests information (e.g., which steps of the tests were successful)
-        as well as script-specific verbose output is printed to the console
-    :return: True, if the list of output files was generated, False otherwise
-    """
-    script_path = str(os.path.join(script_root, (script_name + ".py")))
-    # define script call. The first parameter is the path to (current) python interpreter.
-    # The second parameter is the (absolute) path to executable script
-    script_call = [sys.executable, script_path]
-
-    # add parameters
-    for item in input_param.items():
-        param, val = item
-        script_call.append(param)
-        script_call.append(val)
-
-    # add flags
-    for flag in flags:
-        script_call.append(flag)
-
-    # print("call param", script_call)
-    result = subprocess.run(script_call, capture_output=True, text=True)
-    success = True if result.returncode == 0 else False
-    # print("success:", success)
-
-    # print stdout and stderr returned by script
-    if info_level > 0:
-        print("  - script stdout:", result.stdout)
-        print("  - script stderr:", result.stderr)
-
-    return success
-
-
-def files_exist(files_abs_paths):
-    """
-    Check if files exist
-    :param files_abs_paths abs paths to files
-    :return: True if all the files exist, False otherwise
-    """
-    for file_path in files_abs_paths:
-        if not os.path.exists(file_path):
-            return False
-    return True
-
-
 if __name__ == "__main__":
     main()
+
+    
